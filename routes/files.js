16047dbb463b933dcc2c2016796fc5b6b7155c94
rachit-2702 <rachit.sonthalia2002@gmail.com>
2022-06-24 19:53:26 +0530

    download added

--- routes/files.js
+++ routes/files.js
@@ -43,4 +43,42 @@ router.post('/', (req,res)=>{
 
 })
 
+
+
+router.post('/send', async(req,res)=>{
+
+    const {uuid, emailTo, emailFrom} = req.body;
+
+    if(!uuid || !emailTo || !emailFrom){
+        return res.status(422).send({error: "All fields are required"});
+
+    }
+
+    const file = await File.findOne({uuid: uuid});
+    if(file.sender){
+        return res.status(422).send({error: 'Email already send'});
+    }
+
+    file.sender = emailFrom;
+    file.reciever= emailTo;
+    const response = await file.save();
+
+
+    const sendMail = require("../services/emailService");
+    sendMail({
+        from:emailFrom,
+        to: emailTo,
+        subject: 'File shared',
+        text:`${emailFrom} has shared a file with you`,
+        html: require("../services/emailTemplate")({
+            emailFrom: emailFrom,
+            downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+            size: parseInt(file.size/1000) + ' KB',
+            expires: '24 hours'
+        })
+    })
+return res.send({success:true});
+
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express= require("express");
 const connectDB = require("./config/db");
 const app = express();
-
+const path = require('path');
+require('dotenv').config();
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine','ejs');
+app.use(express.static("public"));
+
+app.use(express.json());
 
 connectDB();
 
@@ -12,6 +17,7 @@ app.use("/api/files", require("./routes/files"))
 
 app.use("/files", require("./routes/show"))
 
+app.use("/files/download", require("./routes/download"))
 
 app.listen(PORT, ()=>{
     console.log("server started on port: "+ PORT);
